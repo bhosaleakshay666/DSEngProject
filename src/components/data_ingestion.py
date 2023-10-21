0c21646004664f58678d8a4b566fb5f2e2142427
bhosaleakshay666 <bhosale.ak@northeastern.edu>
2023-10-20 17:59:59 -0700

    Concat KDE &EDA Transformation and Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,48 +4,100 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 import yfinance as yf
+from datetime import datetime
 
+from src.components.data_transformtion import DataTransformation, DataTransformationConfig
 
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+tick = 'TSLA'
+comp1 = 'TM'
+comp2 = 'GM'
+comp3 = 'F'
+start_date = datetime(2017,1,1)
+end_date = datetime.today()
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+    stock_data_path: str=os.path.join('artifacts',"stock.csv")
+    all_data_path: str=os.path.join('artifacts',"all.csv")
+    comp1_data_path: str=os.path.join('artifacts',"comp1data.csv")
+    comp2_data_path: str=os.path.join('artifacts',"comp2data.csv")
+    comp3_data_path: str=os.path.join('artifacts',"comp3data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
+        logging.info("Entered the stock data ingestion method or component")
         try:
-            df=pd.read_csv(r'notebook\TSLA_Final_Dataset.csv')
+            stock= yf.download(tick, start_date , end_date)
+            stock.reset_index(inplace = True)
+            stock['Company'] = tick
+            stock.to_csv(self.ingestion_config.stock_data_path,index=False,header=True)
             logging.info('Read the dataset as dataframe') 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            comp1_stock= yf.download(comp1, start_date , end_date)
+            comp1_stock.reset_index(inplace = True)
+            comp1_stock['Company'] = comp1
+            comp1_stock.to_csv(self.ingestion_config.comp1_data_path,index=False,header=True)
+            logging.info('Read the 1st competitor dataset as dataframe')
 
-            logging.info("Train test split initiated")
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            comp2_stock= yf.download(comp2, start_date , end_date)
+            comp2_stock.reset_index(inplace = True)
+            comp2_stock['Company'] = comp2
+            comp2_stock.to_csv(self.ingestion_config.comp2_data_path,index=False,header=True)
+            logging.info('Read the 2nd competitor dataset as dataframe')
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            comp3_stock= yf.download(comp3, start_date , end_date)
+            comp3_stock.reset_index(inplace = True)
+            comp3_stock['Company'] = comp3
+            comp3_stock.to_csv(self.ingestion_config.comp3_data_path,index=False,header=True)
+            logging.info('Read the 3rd competitor dataset as dataframe')
+            #concat call from transformation
+            #os.makedirs(os.path.dirname(self.ingestion_config.all_data_path), exist_ok=True)
+            #all = 
+            #all.to_csv(self.ingestion_config.all_data_path,index=False,header=True)
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Transformation Intitated")
+            #train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            
+            #train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            logging.info("Ingestion complete")
+            #test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
 
             return(
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.stock_data_path,
+                self.ingestion_config.comp1_data_path,
+                self.ingestion_config.comp2_data_path,
+                self.ingestion_config.comp3_data_path,
+
+
+
 
             )
         except Exception as e:
             raise CustomException(e,sys)
         
 
+    def concatstocks(self, df):
+        logging.info("Saving Concated Stocks Dataframe")
+
+        try:
+            os.makedirs(os.path.dirname(self.ingestion_config.all_data_path), exist_ok=True)
+            df = df.to_csv(self.ingestion_config.all_data_path,index=False,header=True)
+            logging.info("Ingestion complete")
+            return df
+        except Exception as e:
+            raise CustomException(e,sys)
+
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
+    stock, comp1, comp2, comp3 = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    all, allp=data_transformation.initiate_data_transformation(stock, comp1, comp2, comp3)
+    all = obj.concatstocks(all)
--- src/components/data_transformtion.py
+++ src/components/data_transformtion.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 import numpy as np 
 import pandas as pd
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
+'''from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer'''
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+#from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
@@ -21,5 +21,116 @@ class DataTransformationConfig:
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
+
+    def add_features(self, df):
+        df['MA_10'] = df['Adj Close'].rolling(10).mean()
+        df['MA_20'] = df['Adj Close'].rolling(20).mean() 
+        df['MA_60'] = df['Adj Close'].rolling(60).mean()
+        df['Daily Return'] = df['Adj Close'].pct_change()
+        return df
+        
+    
+        
+    def get_data_transformer_object(self, all_stocks):
+        '''
+        This function si responsible for data trnasformation
+        
+        '''
+        try:
+            '''numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline= Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")'''
+
+            '''preprocessor=Pipeline([
+            ('moving_averages', self.add_features, all_stocks)
+            #('daily_returns', add_daily_returns)
+            ])'''
+            prep_ob= self.add_features(all_stocks)
+            return prep_ob
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+    def initiate_data_transformation(self,stock_path,comp1_path, comp2_path, comp3_path):
+
+        try:
+            stock=pd.read_csv(stock_path)
+            comp1=pd.read_csv(comp1_path)
+            comp2=pd.read_csv(comp2_path)
+            comp3=pd.read_csv(comp3_path)
+
+            logging.info("Reading all data completed")
+
+            logging.info("Obtaining preprocessing object")
+
+            #preprocessing_obj=self.get_data_transformer_object()
+
+            
+            # Concatenate all the stock dataframes into one
+            all_stocks = pd.concat([stock, comp1, comp2, comp3], axis=0)
+            all_stocks.reset_index(drop=True, inplace=True)
+            preprocessing_obj=self.get_data_transformer_object(all_stocks)
+
+            '''input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]'''
+
+
+
+            logging.info(
+                f"Applying preprocessing object on combined dataframe"
+            )
+
+            '''kde_prepr=preprocessing_obj.(all_stocks)'''
+
+            '''kde_arr = np.c_[
+                kde_prepr, np.array(all_stocks)
+            ]'''
+            '''test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]'''
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+
+            return (
+                preprocessing_obj,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
 
    
